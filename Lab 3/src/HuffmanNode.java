a0889b449f0e7ef7a2f5df961772a5a8b8d81b87
dargos82 <142857091+dargos82@users.noreply.github.com>
2024-04-13 10:31:33 -0400

    Updated HuffmanNode

--- Lab 3/src/HuffmanNode.java
+++ Lab 3/src/HuffmanNode.java
@@ -7,17 +7,56 @@
  */
 public class HuffmanNode {
 	
+
+
 	public String value;
 	public int frequency;
 	public HuffmanNode rightChild;
 	public HuffmanNode leftChild;
 	
-	public HuffmanNode(String value, int frequency) {
+	public HuffmanNode(String value, int frequency, HuffmanNode leftChild, HuffmanNode rightChild) {
 		
 		this.value = value;
 		this.frequency = frequency;
+		this.leftChild = null;
 		this.rightChild = null;
-		this.leftChild = null;		
+		
 	} //end HuffmanNode()
+		
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public int getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+	public HuffmanNode mergeNodes(HuffmanNode firstNode, HuffmanNode secondNode) {
+		
+		HuffmanNode newNode = new HuffmanNode(value, frequency, leftChild, rightChild);
+		
+		if(firstNode.value.charAt(0) < secondNode.value.charAt(0)) {
+			
+			this.value = firstNode.value + secondNode.value;
+		} //end if
+		
+		else 
+			this.value = secondNode.value + firstNode.value;
+		
+		this.frequency = firstNode.frequency + secondNode.frequency;
+		
+		this.rightChild = firstNode;
+		this.leftChild = secondNode;
+		
+		return newNode;
+		
+	} //end mergeNodes()
 		
 }
